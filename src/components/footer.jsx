12bf5deb089579ff33e5e90983c5596d5003f47c
YASJUN <37434288+YASJUN@users.noreply.github.com>
2025-01-10 21:34:24 +0900

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
           <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
           &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
           |&nbsp;
-          <CustomLink to="https://www.yorushi.com/ServerStatus/web/" text="ServerStatus" />
+          <CustomLink to="https://status.yorushi.com/" text="ServerStatus" />
         </p>
         <p>
           Copyright&nbsp;&copy;&nbsp;2019&nbsp;-&nbsp;{new Date().getFullYear()}
